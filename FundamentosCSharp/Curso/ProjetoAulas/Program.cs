c93080a57cbfda627516c5426be8db1f21c8ce48
felipepatente <felipepatente@yahoo.com.br>
2024-10-31 23:27:57 -0300

    Trabalhando com variáveis

--- FundamentosCSharp/Curso/ProjetoAulas/Program.cs
+++ FundamentosCSharp/Curso/ProjetoAulas/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
+            int idade = 33;
+            string nomePessoa = "Felipe Patente";
+            decimal valor = 200.99m;
+            double valorDouble = 200.99;
+            float valorFloat = 200.99f;
+            var idadeNova = 34;
+            char flag = 'Y';
+            bool ativo = true;
+
             Console.WriteLine("Hello, World!");
+            Console.WriteLine(idade);
+            Console.WriteLine(nomePessoa);
+            Console.WriteLine(valor);
+            Console.WriteLine(valorDouble);
+            Console.WriteLine(valorFloat);
+            Console.WriteLine(flag);
+            Console.WriteLine(ativo);
         }
     }
 }

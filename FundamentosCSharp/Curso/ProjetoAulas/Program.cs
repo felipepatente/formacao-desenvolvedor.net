50dab78c56a80125a429675811f4a7aa22402fba
felipepatente <felipepatente@yahoo.com.br>
2024-12-19 22:48:37 -0300

    IF/ELSE IF/ELSE - 2

--- FundamentosCSharp/Curso/ProjetoAulas/Program.cs
+++ FundamentosCSharp/Curso/ProjetoAulas/Program.cs
@@ -7,15 +7,35 @@ namespace ProjetoAulas
     {
         static void Main(string[] args)
         {
-            var diaDaSemana = 1;
-            var diaDeTrabalho = true;
+            var diaDaSemana = 4;
+            
 
-            if (diaDaSemana == 0 && diaDeTrabalho)
-                Console.WriteLine("Hoje é domingo");
-            else if (diaDaSemana == 0 && !diaDeTrabalho)
-                Console.WriteLine("Hoje é domingo, mas não é dia de trabalho");
-            else
-                Console.WriteLine("Hoje não é domingo");
+            if(diaDaSemana == 1)
+                Console.WriteLine("Domingo");
+            if (diaDaSemana == 2)
+                Console.WriteLine("Segunda");
+            if (diaDaSemana == 3)
+                Console.WriteLine("Terça");
+            if (diaDaSemana == 4)
+                Console.WriteLine("Quarta");
+            if (diaDaSemana == 5)
+                Console.WriteLine("Quinta");
+            if (diaDaSemana == 6)
+                Console.WriteLine("Sexta");
+            if (diaDaSemana == 7)
+                Console.WriteLine("Sabado");
+
+            //DICIONARIO
+            var diasSemana = new Dictionary<int, string>();
+            diasSemana.Add(1, "Domingo");
+            diasSemana.Add(2, "Segunda");
+            diasSemana.Add(3, "Terça");
+            diasSemana.Add(4, "Quarta");
+            diasSemana.Add(5, "Quinta");
+            diasSemana.Add(6, "Sexta");
+            diasSemana.Add(7, "Sabado");
+
+            Console.WriteLine(diasSemana[diaDaSemana]);
         }
     }
 }

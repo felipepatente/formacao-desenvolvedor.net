5ed64ebac1a3862140fa09c5a734db473a34a2b6
felipepatente <felipepatente@yahoo.com.br>
2025-01-07 22:00:01 -0300

    For

--- FundamentosCSharp/Curso/ProjetoAulas/Program.cs
+++ FundamentosCSharp/Curso/ProjetoAulas/Program.cs
@@ -7,34 +7,12 @@ namespace ProjetoAulas
     {
         static void Main(string[] args)
         {
-            var diaDaSemana = 0;
+            var lista = new List<string>() { "Rafael", "Curso", "Csharp" };
 
-            switch (diaDaSemana) 
-            { 
-                case 0:
-                    Console.WriteLine("Hojé domingo");
-                    break;
-                case 1:
-                    Console.WriteLine("Hoje é segunda");
-                    break;
-                case 2:
-                    Console.WriteLine("Hoje é terça");
-                    break;
-                case 3:
-                    Console.WriteLine("Hoje é quarta");
-                    break;
-                case 4:
-                    Console.WriteLine("Hoje é quinta");
-                    break;
-                case 5:
-                    Console.WriteLine("Hoje é sexta");
-                    break;
-                case 6:
-                    Console.WriteLine("Hoje é sábado");
-                    break;
-                default: 
-                    Console.WriteLine("invalido");
-                    break;
+            for(var i=0; i < lista.Count; i++)
+            {
+                var nome = lista[i];
+                Console.WriteLine(nome);
             }
         }
     }

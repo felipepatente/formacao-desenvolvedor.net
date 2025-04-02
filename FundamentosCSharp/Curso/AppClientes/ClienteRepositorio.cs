f478dada62013c0847b75f1e06dd6a12f17f2333
felipepatente <felipepatente@yahoo.com.br>
2025-04-01 23:11:28 -0300

    Implementando Funcionalidades

---
+++ FundamentosCSharp/Curso/AppClientes/ClienteRepositorio.cs
@@ -0,0 +1,111 @@
+﻿namespace AppClientes
+{
+    public class ClienteRepositorio
+    {
+        public List<Cliente> clientes = new List<Cliente>();
+
+        public void ImprimirCliente(Cliente cliente)
+        {
+            Console.WriteLine($"ID....................: {cliente.Id}");
+            Console.WriteLine($"Nome..................: {cliente.Nome}");
+            Console.WriteLine($"Desconto..............: {cliente.Desconto.ToString("0.00")}");
+            Console.WriteLine($"Data Nascimento.......: {cliente.DataNascimento}");
+            Console.WriteLine($"Data Cadastro.........: {cliente.CadastradoEm}");
+            Console.WriteLine("........................");
+        }
+
+        public void ExcluirCliente()
+        {
+            Console.Clear();
+            Console.WriteLine("Informe o código do cliente: ");
+            var codigo = Console.ReadLine();
+
+            var cliente = clientes.FirstOrDefault(c => c.Id == int.Parse(codigo));
+
+            if (cliente == null)
+            {
+                Console.WriteLine("Cliente não encontrado! [Enter]");
+                Console.ReadKey();
+                return;
+            }
+
+            ImprimirCliente(cliente);
+            clientes.Remove(cliente);
+            Console.WriteLine("Cliente removido com sucesso! [Enter]");
+            Console.ReadKey();
+        }
+
+        public void ExibirClientes()
+        {
+            Console.Clear();
+            clientes.ForEach(cliente => ImprimirCliente(cliente));
+            Console.ReadKey();
+        }
+
+        public void CadastrarCliente()
+        {
+            Console.Write("Nome do cliente: ");
+            var nome = Console.ReadLine();
+            Console.WriteLine(Environment.NewLine);
+
+            Console.Write("Data de nascimento: ");
+            var dataNascimento = DateOnly.Parse(Console.ReadLine());
+            Console.WriteLine(Environment.NewLine);
+
+            Console.Write("Desconto: ");
+            var desconto = Decimal.Parse(Console.ReadLine());
+            Console.WriteLine(Environment.NewLine);
+
+            var cliente = new Cliente();
+            cliente.Id = clientes.Count() + 1;
+            cliente.Nome = nome;
+            cliente.DataNascimento = dataNascimento;
+            cliente.Desconto = desconto;
+            cliente.CadastradoEm = DateTime.Now;
+
+            clientes.Add(cliente);
+
+            Console.WriteLine("Cliente cadastrado com sucesso! [Enter]");
+            ImprimirCliente(cliente);
+        }
+
+        public void EditarCliente()
+        {
+            Console.Clear();
+            Console.WriteLine("Informe o código do cliente: ");
+            var codigo = Console.ReadLine();
+
+            var cliente = clientes.FirstOrDefault(c => c.Id == int.Parse(codigo));
+
+            if (cliente == null) 
+            {
+                Console.WriteLine("Cliente não encontrado! [Enter]");
+                Console.ReadKey();
+                return;
+            }
+
+            ImprimirCliente(cliente);
+
+            Console.Write("Nome do cliente: ");
+            var nome = Console.ReadLine();
+            Console.WriteLine(Environment.NewLine);
+
+            Console.Write("Data de nascimento: ");
+            var dataNascimento = DateOnly.Parse(Console.ReadLine());
+            Console.WriteLine(Environment.NewLine);
+
+            Console.Write("Desconto: ");
+            var desconto = Decimal.Parse(Console.ReadLine());
+            Console.WriteLine(Environment.NewLine);
+            
+            cliente.Nome = nome;
+            cliente.DataNascimento = dataNascimento;
+            cliente.Desconto = desconto;
+            cliente.CadastradoEm = DateTime.Now;
+
+            Console.WriteLine("Cliente alterado com sucesso! [Enter]");
+            ImprimirCliente(cliente);
+            Console.ReadKey();
+        }
+    }
+}
--- FundamentosCSharp/Curso/AppClientes/Program.cs
+++ FundamentosCSharp/Curso/AppClientes/Program.cs
@@ -1,9 +1,17 @@
-﻿namespace AppClientes
+﻿using System.Globalization;
+
+namespace AppClientes
 {
     public class Program
     {
+        static ClienteRepositorio _clienteRepositorio = new ClienteRepositorio();
+
         static void Main(string[] args)
         {
+            var cultura = new CultureInfo("pt-BR");
+            Thread.CurrentThread.CurrentCulture = cultura;
+            Thread.CurrentThread.CurrentUICulture = cultura;
+
             while (true)
             {
                 Menu();
@@ -23,6 +31,54 @@
             Console.WriteLine("4 - Excluir Cliente");
             Console.WriteLine("5 - Sair");
             Console.WriteLine("--------------------");
+
+            EscolherOpcao();
+        }
+
+        static void EscolherOpcao()
+        {
+            Console.WriteLine("Escolha uma opção: ");
+
+            var opcao = Console.ReadLine();
+
+            switch(int.Parse(opcao))
+            {
+                case 1:
+                    {
+                        _clienteRepositorio.CadastrarCliente();
+                        Menu();
+                        break;
+                    }
+                case 2:
+                    {
+                        _clienteRepositorio.ExibirClientes();
+                        Menu();
+                        break;
+                    }
+                case 3:
+                    {
+                        _clienteRepositorio.EditarCliente();
+                        Menu();
+                        break;
+                    }
+                case 4:
+                    {
+                        _clienteRepositorio.ExcluirCliente();
+                        Menu();
+                        break;
+                    }
+                case 5:
+                    {
+                        Environment.Exit(0);
+                        break;
+                    }
+                default:
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Opção Inválida!");
+                        break;
+                    }
+            }
         }
     }
 }

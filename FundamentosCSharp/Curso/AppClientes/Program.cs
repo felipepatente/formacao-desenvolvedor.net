8ea8a73c67a1ba5ab4483e4dcb7309c0c7e2059d
felipepatente <felipepatente@yahoo.com.br>
2025-04-01 22:23:39 -0300

    Criando o menu

--- FundamentosCSharp/Curso/AppClientes/Program.cs
+++ FundamentosCSharp/Curso/AppClientes/Program.cs
@@ -1,10 +1,28 @@
 ﻿namespace AppClientes
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Menu();
+                Console.ReadKey();
+            }
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Cadastro de Clientes");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("1 - Cadastro Cliente");
+            Console.WriteLine("2 - Exibir Clientes");
+            Console.WriteLine("3 - Editar Cliente");
+            Console.WriteLine("4 - Excluir Cliente");
+            Console.WriteLine("5 - Sair");
+            Console.WriteLine("--------------------");
         }
     }
 }

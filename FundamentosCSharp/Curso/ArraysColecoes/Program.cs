828f1d18494123efb3bfc37e523c3ea0bb4f1b7d
felipepatente <felipepatente@yahoo.com.br>
2025-07-22 21:57:23 -0300

    Inicio curso trabalhando com arrays e coleções

---
+++ FundamentosCSharp/Curso/ArraysColecoes/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ArraysColecoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

2fd9ede0d3954960fad0704464533bac1fc3d39a
felipepatente <felipepatente@yahoo.com.br>
2025-09-14 19:47:33 -0300

    Criando um FileStream

--- FundamentosCSharp/Curso/TrabalhandoComArquivos/Program.cs
+++ FundamentosCSharp/Curso/TrabalhandoComArquivos/Program.cs
@@ -4,6 +4,13 @@
     {
         static void Main(string[] args)
         {
+            var enderecoDoArquivo = "contas.txt";
+            var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+
+            var buffer = new byte[1024];
+
+            fluxoDoArquivo.Read(buffer, 0, 1024);
+
             Console.WriteLine("Hello, World!");
         }
     }

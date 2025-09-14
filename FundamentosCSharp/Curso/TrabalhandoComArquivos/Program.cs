4a9d02c335afed8041a14e4bc11837fc4fdd6998
felipepatente <felipepatente@yahoo.com.br>
2025-09-14 20:14:12 -0300

    Método especializado

--- FundamentosCSharp/Curso/TrabalhandoComArquivos/Program.cs
+++ FundamentosCSharp/Curso/TrabalhandoComArquivos/Program.cs
@@ -1,17 +1,30 @@
 ﻿namespace TrabalhandoComArquivos
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             var enderecoDoArquivo = "contas.txt";
+            var numeroDeBytesLidos = -1;
             var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
-
             var buffer = new byte[1024];
 
-            fluxoDoArquivo.Read(buffer, 0, 1024);
+            while (numeroDeBytesLidos != 0)
+            {
+                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                EscreverBuffer(buffer);
+            }
 
             Console.WriteLine("Hello, World!");
+        }
+
+        static void EscreverBuffer(byte[] buffer)
+        {
+            foreach (var meuByte in buffer)
+            {
+                Console.Write(meuByte);
+                Console.Write(" ");
+            }
         }
     }
 }

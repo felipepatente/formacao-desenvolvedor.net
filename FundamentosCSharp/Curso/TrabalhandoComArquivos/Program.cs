208a70649173caea115f6650098a647e6693c39d
felipepatente <felipepatente@yahoo.com.br>
2025-10-14 22:57:29 -0300

    Close e Using

--- FundamentosCSharp/Curso/TrabalhandoComArquivos/Program.cs
+++ FundamentosCSharp/Curso/TrabalhandoComArquivos/Program.cs
@@ -7,17 +7,22 @@ namespace TrabalhandoComArquivos
         static void Main(string[] args)
         {
             var enderecoDoArquivo = "contas.txt";
-            var numeroDeBytesLidos = -1;
-            var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
-            var buffer = new byte[1024];
 
-            while (numeroDeBytesLidos != 0)
+            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
             {
-                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-                EscreverBuffer(buffer);
-            }
+                var numeroDeBytesLidos = -1;
 
-            Console.WriteLine("Hello, World!");
+                var buffer = new byte[1024];
+
+                while (numeroDeBytesLidos != 0)
+                {
+                    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                    EscreverBuffer(buffer);
+                }
+
+                fluxoDoArquivo.Close();
+                Console.WriteLine("Hello, World!");
+            }
         }
 
         static void EscreverBuffer(byte[] buffer)
